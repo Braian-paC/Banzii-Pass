06425365c1598e68ec27500d447451881fdef90a
Braian Baron Gonçalves <braianbbg4@gmail.com>
2024-01-19 16:04:43 -0300

    Organização do Projeto

--- Github.md
+++ Github.md
@@ -1,4 +1,4 @@
-Esse é o primeiro projeto que estou criando no Github, então vou guardar alguns comandos aqui:
+Esse é o primeiro projeto que estou criando no Github, então vou guardar algumas anotações aqui:
 
 git add .
 git status
@@ -6,3 +6,9 @@ git commit -m "nome da alteração"
 git push origin main
 git checkout -b "new-branch"
 git checkout branch
+git merge branch
+
+cd projeto
+git pull
+fork = copiar o projeto de outra pessoa para o seu repositório
+pull request = literal do nome, requere o acesso do dono do projeto para realizar o pull
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-import random, time
-
+import random as rd
 # import de bibliotecas
 
+# funções
 choice = 0
-print('-=-'*15, '\nBanzii Pass'), print('-=-'*15)
-while choice != 2 and choice != 1:
-    choice = int(input('O que você deseja?\n1 -> Gerar uma senha totalmente randômica\n2 -> Randomizar uma senha já existente\n'))
 def senha_random():
     random_characters = "0123456789AaBbCcDdEeFfGgHhJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZzçÇ]}[{?!#$%&*"
     caracteres, pass_origin = 0, ""
-    while caracteres > 15 or caracteres <= 0:
-        caracteres = int(input('Insira o número de caracteres (de 1 a 15) -> '))
+    while caracteres <= 5 or caracteres > 20:
+        caracteres = int(input('Quantos caracteres a senha deverá ter? (de 6 a 20)\n'))
     for c in range(0, caracteres):
-        pass_add = random.choice(random_characters)
+        pass_add = rd.choice(random_characters)
         pass_origin = pass_add + pass_origin
     print(f'Senha: "{pass_origin}"')
 def randomizar_senha():
     pass_old = str(input('Digite sua senha atual: '))
-    pass_new = ''.join(random.sample(pass_old, len(pass_old)))
+    pass_new = ''.join(rd.sample(pass_old, len(pass_old)))
     print(f'Senha randomizada: {pass_new}')
-    print(len(pass_old), len(pass_new))
+
+# mecânica do gerador de senhas
+print('-=-'*15, '\nBanzii Pass'), print('-=-'*15)
+while choice != 2 and choice != 1:
+    choice = int(input('O que você deseja?\n1 -> Gerar uma senha totalmente randômica\n2 -> Randomizar uma senha já existente\n'))
 print('-=-'*15)
 if choice == 1:
     senha_random()
